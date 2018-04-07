5daf169cd834bb1a4d87242c7d4297fbd40799f1
Duck126 <jduck1133@gmail.com>
2018-04-06 21:55:26 -0500

    Omdb Fix

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # liri-node-app
+
+# Instructions ---
+
+# 1 - Copy and paste your own .env file with keys in this format.....
+
+        # Spotify API keys
+
+        SPOTIFY_ID=0
+        SPOTIFY_SECRET=0
+
+        # Twitter API keys
+
+        TWITTER_CONSUMER_KEY=oxmDRqY6E1kC7OTafj80sTqcc
+        TWITTER_CONSUMER_SECRET=y0PIKnijTjNa3sHfZ7Sno8SFExT5PA7tCKgx1ObvBYUvDdaegv
+        TWITTER_ACCESS_TOKEN_KEY=979095234884853760-GDLH4W7GB2o0RqyVwSN08t9vtMuPChW
+        TWITTER_ACCESS_TOKEN_SECRET=nKLxQfG7rpuEikajnRrSctSCwbkG0T07oSsAdBRbn6fBm
+
+# 2 - On the command line in gitbash type-- node liri  ....And any one of the following commands --->
+
+        node liri addTweet -any text that you want in your tweet-
+
+        node liri seeTweets
+
+        node liri movieThis -any movie you would like to see-
--- liri.js
+++ liri.js
@@ -4,6 +4,11 @@ var keys = require("./keys.js");
 var Twitter = require('twitter');
 var client = new Twitter(keys.twitter);
 var fs = require('fs-extra');
+//var request = require('request');
+var request = require('request-promise');
+
+
+//<----------------Twitter----------------->
 
 //Post a Tweet... in Gbash type node liri -Your selected text for the tweet- 
 // Hit enter and post the tweet! Dont forget to use your own .env file for keys!
@@ -40,20 +45,9 @@ function addTweet() {
 
 };
 
-//Logs each command that liri uses into log.txt
-
-function logCommand() {
-
-  var command = JSON.stringify(process.argv[1] + " " + process.argv[2]);
-
-  var commandTxt = "Liri Command: " + command.slice(61) + "\n ------------------------------\n ";
-  
-  fs.appendFile('log.txt', commandTxt, function (err){
-    if(err) throw err;
-  })
-}
 //Retrieve Last 20 Tweets, this uses the raw JSON from our tweets, puts them in a loop and filters...
-//... out the unwanted data and leaves us with the Tweet Text and Time Posted. 
+//... out the unwanted data and leaves us with the Tweet Text and Time Posted
+
 function seeTweets() {
 
   client.get('statuses/user_timeline.json?count=20', function (error, tweets, response) {
@@ -70,8 +64,51 @@ function seeTweets() {
       });
     }
     console.log('Saved!');
-    
+
   });
+}
+
+//<----------------OMDB----------------->
+
+function movieThis() {
+
+  let movie = process.argv.slice(3).join('+');
+
+  
+  if (movie === '') {
+    movie = 'Mr Nobody';
+  }
+//65f2aeec
+
+  request('http://www.omdbapi.com/?t=' + movie + '&r=json&plot=short&apikey=8b7b736c')
+    .then(response => {
+      var data = JSON.parse(response);
+
+      var movieData={
+        "Title: ": data.Title,
+        "Year: ": data.Year,
+        "IMDB Rating: ": data.Ratings[0],
+        "Rotten Tomatoes: ": data.Ratings[1],
+        "Country: ": data.Country,
+        "Language: ": data.Language,
+        "Plot: ": data.Plot,
+        "Actors: ": data.Actors
+      }
+      console.log(movieData);
+    });
+}
+
+//Logs each command that liri uses into log.txt
+
+function logCommand() {
+
+  var command = JSON.stringify(process.argv[1] + " " + process.argv[2]);
+
+  var commandTxt = "Liri Command: " + command.slice(61) + "\n ------------------------------\n ";
+
+  fs.appendFile('log.txt', commandTxt, function (err) {
+    if (err) throw err;
+  })
 }
 
 
@@ -81,4 +118,7 @@ if (process.argv[2] === "seeTweets") {
 } else if (process.argv[2] === "addTweet") {
   logCommand();
   addTweet();
+} else if (process.argv[2] === "movieThis") {
+  movieThis();
+  logCommand();
 }
